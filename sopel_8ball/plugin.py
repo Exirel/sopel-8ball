6ed34cca399e19e4373f14273ee4f46aad37147b
xnaas <me@xnaas.info>
2022-01-19 11:05:45 -0600

    139
    Co-authored-by: Florian Strzelecki <florian.strzelecki@gmail.com>

--- sopel_8ball/plugin.py
+++ sopel_8ball/plugin.py
@@ -27,7 +27,7 @@ def configure(settings: Config) -> None:
     managers.manager.configure(settings)
 
 
-@plugin.commands('8ball', '8')
+@plugin.commands('8ball', '8b')
 @plugin.example('.8ball Do androids dream of electric sheep?', user_help=True)
 def query(bot: SopelWrapper, trigger: Trigger) -> None:
     """Query the magic 8 ball for an answer."""
