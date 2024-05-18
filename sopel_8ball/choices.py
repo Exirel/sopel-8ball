62725b340681483dc6f87bfde7337597e5e1ffee
dgw <dgw@technobabbl.es>
2024-05-17 19:56:31 -0500

    choices: add YesNo provider

--- sopel_8ball/choices.py
+++ sopel_8ball/choices.py
@@ -192,3 +192,11 @@ class Weeaball(AbstractChoiceProvider):
             "Muri. ▄█▀█●",
             "Zannen. _|￣|○",
         )
+
+
+class YesNo(AbstractChoiceProvider):
+    def choices(self) -> Tuple[str, ...]:
+        return (
+            "Yes.",
+            "No.",
+        )
